a30632fb0dfa52f81d5fc3148f1ec26e7b69273e
Jmart5564 <Jmart5564@gmail.com>
2022-09-19 14:32:59 -0700

    added blogs function to services and added useEffect hook function to hooks

---
+++ src/hooks/Blogs.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import { getBlogs } from '../services/blogs';
+
+export function useBlogs() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    async function fetchBlogs() {
+      const data = await getBlogs();
+      setBlogs(data);
+    }
+    fetchBlogs();
+  }, []);
+
+  return blogs;
+}
